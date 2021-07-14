e1cc8630dedf465835bae196c779f071b5866011
niloofaramerian <niloofar.amerian77@gmail.com>
2021-07-14 09:44:08 +0430

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:weather_forecast_app/weather_forecast.dart';
+
+void main() => runApp(new MaterialApp(
+  home: WeatherForecast(),
+));
---
+++ lib/weather_forecast.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class WeatherForecast extends StatefulWidget {
+  const WeatherForecast({Key? key}) : super(key: key);
+
+  @override
+  _WeatherForecastState createState() => _WeatherForecastState();
+}
+
+class _WeatherForecastState extends State<WeatherForecast> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Forecast"),
+      ),
+    );
+  }
+}
